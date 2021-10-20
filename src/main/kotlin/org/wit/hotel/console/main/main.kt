742d94c71f1c4dae060ec0e2b33311d315335fc8
Denis <60179716+pteamindark@users.noreply.github.com>
2021-10-20 20:39:24 +0100

    Main to controller().start()

--- src/main/kotlin/org/wit/hotel/console/main/main.kt
+++ src/main/kotlin/org/wit/hotel/console/main/main.kt
@@ -8,29 +8,12 @@ import org.wit.hotel.console.controllers.HotelController
 private val logger= KotlinLogging.logger {}
 
 
-
+val hotels = HotelMemStore()
 val hotelView = HotelView()
 val controller = HotelController()
 
-fun main(args: Array<String>){
-    println("Hotel App")
-    logger.info {"Starting Hotel Console"}
-
-    var input: Int
-
-    do {
-        input = hotelView.menu()
-        when(input) {
-            1 -> controller.addHotel()
-            2 -> controller.updateHotel()
-            3 -> controller.listHotel()
-            4 -> controller.searchHotel()
-            -1 -> println("Quiting the App")
-            else -> println("Invalid Option")
-        }
-        println()
-    } while (input != -1)
-    logger.info { "Exiting Hotel Console" }
+fun main(args: Array<String>) {
+HotelController().start()
 }
 
 
